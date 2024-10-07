23dcd78264d4bcba1cf32db25a8d46e79ad2cd50
paxxchal <paschalgabrielacj8@gmail.com>
2024-10-08 00:29:14 +0100

    started desgining admin dashboard and implemented next.js radial charts with recharts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        caesarSky: "#9F98D8",
+        caesarSkyLight: "#C4BFE8",
+        caesarBlue: "#4335B2",
+        caesarBlueLight: "#6958D4",
+        caesarPink: "#F652A0",
+        caesarPinkLight: "#FF66C4",
+        caesarRed: "#E63946",
+      },
     },
   },
   plugins: [],
